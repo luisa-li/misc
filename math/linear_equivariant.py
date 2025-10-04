786fae7afe814cafda3ca4192ff8939ca47381aa
Luisa <103768169+luisa-li@users.noreply.github.com>
2025-10-04 01:11:47 -0400

    rah

--- math/linear_equivariant.py
+++ math/linear_equivariant.py
@@ -1,7 +1,9 @@
 import sympy as sp
 
-# defining the generators of the group D3
+def compute_constraints(mat):
+    return sp.kronecker_product(mat, (mat.inv()).T) - sp.eye(36)
 
+# defining the generators of the group D3
 rho_r = sp.Matrix([
     [0, 0, 1, 0, 0, 0],
     [1, 0, 0, 0, 0, 0],
@@ -19,13 +21,6 @@ rho_f = sp.Matrix([
     [0, 0, 1, 0, 0, 0],
     [0, 1, 0, 0, 0, 0]
 ])
-
-# computing the constraint for each of the generators
-
-
-def compute_constraints(mat):
-    return sp.kronecker_product(mat, (mat.inv()).T) - sp.eye(36)
-
 
 cons1 = compute_constraints(rho_r)
 cons2 = compute_constraints(rho_f)
@@ -45,7 +40,9 @@ rho_r2 = rho_r * rho_r
 rho_rf = rho_r * rho_f
 rho_r2f = rho_r2 * rho_f
 
-# checking commutativity 
+# checking commutativity
 for w in W_list:
-    for g in [rho_e, rho_r, rho_r2, rho_f, rho_rf, rho_r2f]: 
+    for g in [rho_e, rho_r, rho_r2, rho_f, rho_rf, rho_r2f]:
         assert w * g == g * w
+
+print(W_list)
